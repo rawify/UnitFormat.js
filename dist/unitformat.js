92e5ba50e9c1ad13699da5cbee11d5baf361c6da
Robert Eisele <robert@xarg.org>
2025-08-13 09:16:48 +0200

    Code Review

--- dist/unitformat.js
+++ dist/unitformat.js
@@ -1,49 +1,67 @@
 'use strict';
 
 var units = {
-  2: {
-    "E": 60,
-    "P": 50,
-    "T": 40,
-    "G": 30,
-    "M": 20,
-    "k": 10
+  '2': {
+    'E': 60,
+    'P': 50,
+    'T': 40,
+    'G': 30,
+    'M': 20,
+    'k': 10
   },
-  10: {
-    "E": 18,
-    "P": 15,
-    "T": 12,
-    "G": 9,
-    "M": 6,
-    "k": 3,
-    "h": 2,
-    "d": -1,
-    "c": -2,
-    "m": -3,
-    "u": -6,
-    "μ": -6,
-    "n": -9,
-    "p": -12,
-    "f": -15,
-    "a": -18
+  '10': {
+    'E': 18,
+    'P': 15,
+    'T': 12,
+    'G': 9,
+    'M': 6,
+    'k': 3,
+    'h': 2,
+    'd': -1,
+    'c': -2,
+    'm': -3,
+    'u': -6,
+    'μ': -6,
+    'n': -9,
+    'p': -12,
+    'f': -15,
+    'a': -18
   }
 };
 
+function round3(x) {
+  return Math.round(x * 1000) / 1000;
+}
+
+/**
+ * Format a number with engineering prefixes.
+ * @param {number} num
+ * @param {string=} baseUnit   e.g. "B", "m", "" (default)
+ * @param {string=} suffixes   allowed symbols in priority order (default "kMGTPE")
+ * @param {number=} base       10 or 2 (default 10)
+ * @returns {string}
+ */
 function UnitFormat(num, baseUnit, suffixes, base) {
 
-  if (baseUnit === undefined || baseUnit === null) {
+  if (baseUnit == null) {
     baseUnit = "";
   }
 
-  if (suffixes === undefined || suffixes === null) {
+  if (suffixes == null) {
     suffixes = "kMGTPE";
   }
 
-  if (base === undefined || base === null) {
+  if (base == null) {
     base = 10;
   } else if (units[base] === undefined) {
     throw new Error("Not supported base " + base);
   }
+
+  // Zero fast-path
+  if (num === 0) return "0" + baseUnit;
+
+  var sign = "";
+  if (num < 0) { sign = "-"; num = -num; }
 
   var bestSuffix = "";
   var bestScore = num; // num / b^0
@@ -60,7 +78,7 @@ function UnitFormat(num, baseUnit, suffixes, base) {
       throw new Error("Unknown unit " + t);
     }
   }
-  return Number(bestScore.toFixed(3)) + bestSuffix + baseUnit;
+  return sign + round3(bestScore) + bestSuffix + baseUnit;
 }
 
 Object.defineProperty(UnitFormat, "__esModule", { 'value': true });
